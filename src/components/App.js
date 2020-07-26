8b906d75c1e1afd1b0cee257b3b9f4506fb8db51
Nuriddin <nuriddin_2468@mail.ru>
2020-07-26 13:36:05 +0500

    Image Api implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import unsplash from "../api/unsplash";
+
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        console.log(term);
+    state = {
+        images: []
+    };
+
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+                params: {query: term},
+            });
+        this.setState({images: response.data.results});
     }
 
     render() {
@@ -12,6 +22,7 @@ class App extends React.Component {
                 <SearchBar
                     onSubmit={this.onSearchSubmit}
                 />
+                <ImageList images={this.state.images}/>
             </div>
         );
     }
